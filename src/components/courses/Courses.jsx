313e51d4c52ced2ee9f52dd21f551edeb9d7f08e
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-17 00:08:14 -0300

    Center the courses

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -4,11 +4,17 @@ import Course from './Course';
 const Courses = () => {
   return (
     <>
-      <h2>Courses</h2>
-      <Course
-        url={'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0'}
-        title={'Blockchain32hrs'}
-      />
+      <h2 className='px-8 pt-8 text-3xl font-thin'>Cursos</h2>
+      <div className='flex flex-wrap justify-center'>
+        <Course
+          url={'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0'}
+          title={'Curso Full Stack Blockchain'}
+        />
+        <Course
+          url={'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0'}
+          title={'Blockchain 32hrs Course'}
+        />
+      </div>
     </>
   );
 };
