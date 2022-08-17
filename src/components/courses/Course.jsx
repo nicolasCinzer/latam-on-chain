99f49087392295a1d5dd956c6bb3a44da012d9e5
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-17 00:07:55 -0300

    Desing in progress

--- src/components/courses/Course.jsx
+++ src/components/courses/Course.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import Stack from './Stack';
+
+/* Icons */
 import { SiSolidity, SiJavascript } from 'react-icons/si';
 import { TbBrandNextjs } from 'react-icons/tb';
 import { FaHardHat } from 'react-icons/fa';
 
 const Course = ({ url, title }) => {
   return (
-    <div className='grid justify-items-center'>
-      <h3 className='mb-4 text-3xl'>Blockchain 32hrs Course</h3>
+    <div className='grid justify-items-center shadow-xl bg-white my-8  py-4 w-3/4'>
+      <h3 className='mb-4 text-3xl font-thin'>{title}</h3>
       <iframe
         src={url}
         width='560'
@@ -19,17 +21,17 @@ const Course = ({ url, title }) => {
       />
       <Stack
         icons={[
-          <a href={'https://docs.soliditylang.org/en/latest/'}>
-            <SiSolidity key='solidity' />
+          <a href={'https://docs.soliditylang.org/en/latest/'} key='solidity'>
+            <SiSolidity />
           </a>,
-          <a href={'https://nextjs.org/'}>
-            <TbBrandNextjs key='nextjs' />
+          <a href={'https://nextjs.org/'} key='nextjs'>
+            <TbBrandNextjs />
           </a>,
-          <a href={'https://www.javascript.com/'}>
-            <SiJavascript key='javascript' />
+          <a href={'https://www.javascript.com/'} key='javascript'>
+            <SiJavascript />
           </a>,
-          <a href={'https://hardhat.org/'}>
-            <FaHardHat key='hardhat' />
+          <a href={'https://hardhat.org/'} key='hardhat'>
+            <FaHardHat />
           </a>,
         ]}
       />
