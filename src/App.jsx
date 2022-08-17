ecc8b3f49da544accb33b8854703a5316eedcf17
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-16 23:41:48 -0300

    Change the bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import './App.css';
 import Courses from './components/courses/Courses';
 
 function App() {
   return (
-    <div className='p-0 m-0 flex-column'>
+    <div className='p-0 m-0 flex-column bg-slate-200 h-screen'>
       <Courses />
     </div>
   );
