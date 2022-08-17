5b9835a91b0328eb490b622efe3cc7257646a150
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-17 00:07:08 -0300

    Change heigt to 100%

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Courses from './components/courses/Courses';
 
 function App() {
   return (
-    <div className='p-0 m-0 flex-column bg-slate-200 h-screen'>
+    <div className='p-0 m-0 flex-column bg-slate-200 h-full'>
       <Courses />
     </div>
   );
