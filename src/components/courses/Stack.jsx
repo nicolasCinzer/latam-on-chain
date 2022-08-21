ba8dc87bdf67fd64b8bb701a359b028aaad9daf6
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-21 20:52:39 -0300

    .

--- src/components/courses/Stack.jsx
+++ src/components/courses/Stack.jsx
@@ -2,11 +2,19 @@ import React from 'react';
 
 const Stack = ({ icons }) => {
   return (
-    <div className='flex justify-between w-1/2 py-4'>
-      <div className='grid place-content-center ml-4 mr-12 border-r-2 border-b-2 px-4 pb-4'>
-        <div className='flex justify-center m-4 text-2xl font-thin border-b border-slate-400'>Stack</div>
-        <div className='grid grid-cols-3 gap-x-5 gap-y-5 text-2xl mb-8 w-36'>{icons}</div>
-        <button className='border rounded-xl border-slate-400 hover:shadow-lg'>Ver mas</button>
+    <div className='flex justify-between w-1/2 py-4 sm:grid sm:grid-cols-1'>
+      <div className='grid place-content-center ml-4 mr-12 border-r-2 border-b-2 px-4 pb-4 sm:m-0 sm:mb-5 sm:grid-cols-2'>
+        <div>
+          <div className='flex justify-center m-4 text-2xl font-thin border-b border-slate-400'>
+            Stack
+          </div>
+          <div className='grid grid-cols-3 gap-x-5 gap-y-5 text-2xl mb-8 w-36'>
+            {icons}
+          </div>
+        </div>
+        <button className='border rounded-xl border-slate-400 hover:shadow-lg'>
+          Ver mas
+        </button>
       </div>
       <div className='p-4 border-r-2 border-b-2'>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
