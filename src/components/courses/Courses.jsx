d97d304e4146ad280a52c3c28304c2bda5cd646c
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-22 19:11:00 -0300

    responsive changes and colors

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -4,11 +4,11 @@ import Course from './Course';
 const Courses = () => {
   return (
     <>
-      <h2 className='px-8 pt-8 text-3xl font-thin'>Cursos</h2>
-      <div className='flex flex-wrap justify-center'>
+      <h2 className='px-8 py-8 text-3xl font-thin border-b-2 border-cyan-500'>Cursos</h2>
+      <div className='grid grid-cols-1 place-items-center'>
         <Course
           url={'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0'}
-          title={'Curso Full Stack Blockchain'}
+          title={'Full Stack Blockchain'}
         />
         <Course
           url={'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0'}
--- src/components/courses/Stack.jsx
+++ src/components/courses/Stack.jsx
@@ -2,13 +2,19 @@ import React from 'react';
 
 const Stack = ({ icons }) => {
   return (
-    <div className='flex justify-between w-1/2 py-4'>
-      <div className='grid place-content-center ml-4 mr-12 border-r-2 border-b-2 px-4 pb-4'>
-        <div className='flex justify-center m-4 text-2xl font-thin border-b border-slate-400'>Stack</div>
-        <div className='grid grid-cols-3 gap-x-5 gap-y-5 text-2xl mb-8 w-36'>{icons}</div>
-        <button className='border rounded-xl border-slate-400 hover:shadow-lg'>Ver mas</button>
+    <div className='grid grid-cols-1 place-content-center w-3/4 lg:py-4 lg:flex lg:justify-between'>
+      <div className='grid place-content-center mx-4 px-4 lg:pb-4'>
+        <div className='flex justify-center m-4 pd-1 text-2xl font-thin border-b border-cyan-200'>
+          Stack
+        </div>
+        <div className='grid grid-cols-3 gap-x-5 gap-y-5 text-2xl mb-8 w-36'>
+          {icons}
+        </div>
+        <button className='border rounded-xl border-cyan-200 hover:shadow-lg'>
+          Ver mas
+        </button>
       </div>
-      <div className='p-4 border-r-2 border-b-2'>
+      <div className='p-4 text-center'>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
         quasi, magni recusandae eaque quod quisquam, autem doloribus at
         accusamus aperiam sequi! Veniam qui sequi odit atque dolorum expedita,
