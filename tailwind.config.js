ece556f8db8f901e4bf1892c060d54e3f050c84f
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-22 19:10:14 -0300

    jetbrains mono font added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      jetbrains: ['"JetBrains Mono"', 'ui-sans-serif'],
+    },
   },
   plugins: [],
-}
+};
