14d8a07a347c7a441cf2fe0952582d082b708813
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-22 19:10:28 -0300

    change the bg color

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Courses from './components/courses/Courses';
 
 function App() {
   return (
-    <div className='p-0 m-0 flex-column bg-slate-200 h-full'>
+    <div className='p-0 m-0  bg-gradient-to-r from-cyan-600 to-cyan-900 h-full font-jetbrains text-cyan-200'>
       <Courses />
     </div>
   );
