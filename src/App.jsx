efeebb0f259275f201dcdc1b3837aa4a0798690c
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-24 19:54:09 -0300

    Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { Route } from 'wouter';
+
+import Navbar from './components/navbar/Navbar';
 import Courses from './components/courses/Courses';
 
 function App() {
   return (
-    <div className='p-0 m-0  bg-gradient-to-r from-cyan-600 to-cyan-900 h-full font-jetbrains text-cyan-200'>
-      <Courses />
+    <div className='p-0 m-0 bg-gradient-to-r from-cyan-600 to-cyan-900 h-full font-jetbrains text-cyan-200'>
+      <Navbar />
+      <Route path='/cursos' component={Courses} />
     </div>
   );
 }
