d5eff95e19fc36ac036c9131777a185fc62031f3
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-24 23:57:54 -0300

    font??? new color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    fontFamily: {
-      jetbrains: ['"JetBrains Mono"', 'ui-sans-serif'],
+    extend: {
+      fontFamily: {
+        jetbrains: ['"Cascadia Code PL"', 'sans'],
+      },
+    },
+    colors: {
+      ocean: {
+        50: '#E1FAFE',
+        100: '#BEF4FD',
+        200: '#83E9FC',
+        300: '#42DEFA',
+        400: '#07D0F3',
+        500: '#059AB4',
+        600: '#047B90',
+        700: '#035D6D',
+        800: '#02404A',
+        900: '#011E23',
+      },
     },
   },
   plugins: [],
