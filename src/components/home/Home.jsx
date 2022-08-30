ece15d8c074a986a323c877aa43578ca3c4e0558
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-30 19:53:55 -0300

    Home description

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,14 +2,11 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='grid grid-cols-2 h-screen'>
-      <div>
-        <div className='text text-4xl'>LATAM ON CHAIN</div>
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
-          molestias! Totam facere nisi unde ut ratione fugit repellendus cum
-          molestias corrupti, nam enim, ea culpa repellat quae, id
-          necessitatibus. Dolore!
+    <div className='grid grid-cols-2 p-8 h-screen'>
+      <div className='flex flex-col pr-8 pt-16'>
+        <div className='text-6xl py-4 px-8 mb-4 w-fit bg-emerald-400/60 rounded-3xl'>#LATAMonCHAIN</div>
+        <div className='text-xl pl-4'>
+          LATAM on Chain es un proyecto open-source con el objetivo de recopilar contenido sobre Web3 en español para la comunidad habla hispana.
         </div>
       </div>
       <div>Blockchain Developer</div>
