99d443c2b2f5c189c991811a8a9094367b59453e
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-08-30 06:47:14 -0300

    Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route } from 'wouter';
 
 import Navbar from './components/navbar/Navbar';
+import Home from './components/home/Home';
 import Courses from './components/courses/Courses';
 
 function App() {
   return (
     <div className='p-0 m-0 bg-white h-full'>
       <Navbar />
+      <Route path='/' component={Home} />
       <Route path='/cursos' component={Courses} />
     </div>
   );
---
+++ src/components/home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className='grid grid-cols-2 h-screen'>
+      <div>
+        <div className='text text-4xl'>LATAM ON CHAIN</div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia,
+          molestias! Totam facere nisi unde ut ratione fugit repellendus cum
+          molestias corrupti, nam enim, ea culpa repellat quae, id
+          necessitatibus. Dolore!
+        </div>
+      </div>
+      <div>Blockchain Developer</div>
+    </div>
+  );
+};
+
+export default Home;
