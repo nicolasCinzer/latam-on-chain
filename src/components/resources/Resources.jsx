d682ce70f0081aeee9ce884c581b7090b88c5278
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-08 00:06:47 -0300

    edit ids

--- src/components/resources/Resources.jsx
+++ src/components/resources/Resources.jsx
@@ -11,21 +11,21 @@ const Resources = () => {
       <div className='pt-8 pb-4 px-8 text-2xl'>Ultimos Posteos</div>
       <div className='grid grid-cols-1 px-4'>
         <Resource
-          id={1}
+          id={'que-es-blockchain'}
           topic={'Blockchain'}
           icon={<SiHiveBlockchain />}
           title={'Que es Blockchain?'}
           date={'6 de Sep 2022'}
         />
         <Resource
-          id={2}
+          id={'por-que-es-buena-idea-aprender-solidity-para-este-2023'}
           topic={'Solidity'}
           icon={<SiHiveBlockchain />}
           title={'Por que es buena idea aprender Solidity para este 2023?'}
           date={'6 de Sep 2022'}
         />
         <Resource
-          id={3}
+          id={'que-es-bitcoin'}
           topic={'Bitcoin'}
           icon={<SiHiveBlockchain />}
           title={'Que es Bitcoin?'}
