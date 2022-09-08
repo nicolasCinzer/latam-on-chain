ea400f4292ee25563ab7390606c3e12d28785bf6
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-07 23:30:21 -0300

    Added key to Course Component

--- src/components/courses/Courses.jsx
+++ src/components/courses/Courses.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
 import courses from '../../services/coursesArray';
-import Course from './course/Course';
+import Course from './course';
 
 const Courses = () => {
   return (
     <>
       <div className='grid grid-cols-1 xl:grid-cols-2 place-items-center'>
         {courses.map(({ url, title }) => (
-          <Course url={url} title={title} />
+          <Course key={title} url={url} title={title} />
         ))}
       </div>
     </>
