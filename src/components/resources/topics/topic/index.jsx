340afdde81cd0727fe675d8a2ae8b3e4dc24eaf4
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-08 18:00:58 -0300

    hOVER

--- src/components/resources/topics/topic/index.jsx
+++ src/components/resources/topics/topic/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Topic = ({ topic }) => {
   return (
-    <div className='w-fit px-3 bg-emerald-300/60 rounded-3xl cursor-pointer'>{topic}</div>
+    <div className='w-fit px-3 bg-emerald-300/60 rounded-3xl transition-all hover:scale-110 cursor-pointer'>{topic}</div>
   );
 };
 
