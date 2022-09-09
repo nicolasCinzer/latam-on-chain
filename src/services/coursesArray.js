ca82b5ce9d2d7e956cfe5ae5b1c80ed8143595bc
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-09 09:09:00 -0300

    Courses

--- src/services/coursesArray.js
+++ src/services/coursesArray.js
@@ -2,18 +2,16 @@ let courses = [
   {
     url: 'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0',
     title: 'Full Stack Blockchain',
+    stack: ['solidity', 'nextjs', 'javascript', 'hardhat', 'tailwindcss'],
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Lorem ipsum dolor, sit amet consectetur adipisicing elit Lorem ipsum dolor, sit amet consectetur adipisicing elit ',
   },
   {
     url: 'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0',
     title: 'Blockchain 32hrs Course',
-  },
-  {
-    url: 'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0',
-    title: 'Blockchain 32hrs Course',
-  },
-  {
-    url: 'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0',
-    title: 'Blockchain 32hrs Course',
+    stack: ['solidity', 'nextjs', 'javascript', 'hardhat', 'tailwindcss'],
+    description:
+      'Lorem ipsum dolor, sit amet consectetur adipisicing elit Lorem ipsum dolor, sit amet consectetur adipisicing elit Lorem ipsum dolor, sit amet consectetur adipisicing elit ',
   },
 ];
 
