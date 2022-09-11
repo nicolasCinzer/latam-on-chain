f62d331bf37cbc2cb02d12179e585265b139556c
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-11 18:39:10 -0300

    delete console.log

--- src/components/resources/posts/index.jsx
+++ src/components/resources/posts/index.jsx
@@ -3,8 +3,6 @@ import React from 'react';
 import Post1 from './que-es-blockchain';
 
 const Posts = ({ id }) => {
-  console.log(id);
-
   return <>{id === 'que-es-blockchain' ? <Post1 /> : <></>}</>;
 };
 
