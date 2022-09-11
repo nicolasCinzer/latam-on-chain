ddd0d323334b44a36e0e39dee1366d94a8f46f08
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-11 18:39:54 -0300

    Logic from course bubble changed

--- src/services/coursesArray.js
+++ src/services/coursesArray.js
@@ -1,15 +1,15 @@
 let courses = [
   {
-    url: 'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0',
+    url: 'https://www.youtube.com/watch?v=gyMwXuJrbJQ',
     title: 'Full Stack Blockchain',
     stack: ['solidity', 'nextjs', 'javascript', 'hardhat', 'tailwindcss'],
     description:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit Lorem ipsum dolor, sit amet consectetur adipisicing elit Lorem ipsum dolor, sit amet consectetur adipisicing elit ',
   },
   {
-    url: 'https://www.youtube.com/embed/gyMwXuJrbJQ?start=0',
+    url: 'https://www.youtube.com/watch?v=FAMWIoKvfRs',
     title: 'Blockchain 32hrs Course',
-    stack: ['solidity', 'nextjs', 'javascript', 'hardhat', 'tailwindcss'],
+    stack: ['solidity', 'hardhat', 'tailwindcss'],
     description:
       'Lorem ipsum dolor, sit amet consectetur adipisicing elit Lorem ipsum dolor, sit amet consectetur adipisicing elit Lorem ipsum dolor, sit amet consectetur adipisicing elit ',
   },
