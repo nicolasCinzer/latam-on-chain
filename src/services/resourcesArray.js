2ee87f347c5afe077ef504c9331e6242c29bf9ca
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-11 22:13:34 -0300

    Resources file

---
+++ src/services/resourcesArray.js
@@ -0,0 +1,27 @@
+import { SiHiveBlockchain, SiSolidity, SiBitcoin } from 'react-icons/si';
+
+const resources = [
+  {
+    id: 'que-es-blockchain',
+    topic: 'Blockchain',
+    icon: <SiHiveBlockchain />,
+    title: 'Que es Blockchain?',
+    date: '6 de Sep 2022',
+  },
+  {
+    id: 'por-que-es-buena-idea-aprender-solidity-para-este-2023',
+    topic: 'Solidity',
+    icon: <SiSolidity />,
+    title: 'Por que es buena idea aprender Solidity para este 2023?',
+    date: '6 de Sep 2022',
+  },
+  {
+    id: 'que-es-bitcoin',
+    topic: 'Bitcoin',
+    icon: <SiBitcoin />,
+    title: 'Que es Bitcoin?',
+    date: '6 de Sep 2022',
+  },
+];
+
+export default resources;
