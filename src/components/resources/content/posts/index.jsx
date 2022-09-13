157a0ddd1e5a447dcc7702301376948c85f8428b
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-13 00:03:41 -0300

    Delete useless

--- src/components/resources/content/posts/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import Post1 from './que-es-blockchain';
-
-const Posts = ({ id }) => {
-  return <>{id === 'que-es-blockchain' ? <Post1 /> : <></>}</>;
-};
-
-export default Posts;
