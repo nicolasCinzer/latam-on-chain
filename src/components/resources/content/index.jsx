6d40f525977d4a8f7b4fef397dace68be14dd4e2
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-13 00:48:05 -0300

    .

--- src/components/resources/content/index.jsx
+++ src/components/resources/content/index.jsx
@@ -5,7 +5,17 @@ import Post1 from './posts/que-es-blockchain';
 const Content = ({ params }) => {
   const { id } = params;
 
-  return <>{id === 'que-es-blockchain' ? <Post1 /> : <></>}</>;
+  return (
+    <>
+      {id === 'que-es-blockchain' ? <Post1 /> : <></>}
+      {id === 'por-que-es-buena-idea-aprender-solidity-para-este-2023' ? (
+        <div>solidity</div>
+      ) : (
+        <></>
+      )}
+      {id === 'que-es-bitcoin' ? <div>bitcoin</div> : <></>}
+    </>
+  );
 };
 
 export default Content;
