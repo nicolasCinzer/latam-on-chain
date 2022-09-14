ada6ba9c6da6e2b917b6949f5463e4e31a2f06fd
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-14 02:00:00 -0300

    Style navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,25 +1,48 @@
-import { Link } from 'wouter';
+import { Link, useLocation } from 'wouter';
 
 const Navbar = () => {
+  const [path] = useLocation();
+
   return (
     <div className='flex justify-between p-4 bg-white shadow'>
-      <Link to='/'>LATAM ON CHAIN</Link>
-      <div className=''>
+      <Link
+        to='/'
+        className={`transition-all duration-300 ${
+          path === '/'
+            ? 'pl-1 border-b-2 border-l-2 border-emerald-400 shadow-md'
+            : 'hover:text-emerald-700'
+        }`}
+      >
+        LATAM ON CHAIN
+      </Link>
+      <div>
         <Link
           to='/cursos'
-          className='mx-6 transition-all duration-300 hover:text-emerald-700'
+          className={`mx-6 transition-all duration-300 ${
+            path === '/cursos'
+              ? 'px-2 py-1 bg-emerald-400/60 rounded-3xl '
+              : 'hover:text-emerald-700'
+          } `}
         >
           CURSOS
         </Link>
         <Link
           to='/recursos'
-          className='mx-6 transition-all duration-300 hover:text-emerald-700'
+          className={`mx-6 transition-all duration-300 ${
+            path === '/recursos'
+              ? 'px-2 py-1 bg-emerald-400/60 rounded-3xl '
+              : 'hover:text-emerald-700'
+          } `}
         >
           RECURSOS
         </Link>
         <Link
           to='/faqs'
-          className='mx-6 transition-all duration-300 hover:text-emerald-700'
+          className={`mx-6 transition-all duration-300 ${
+            path === '/faqs'
+              ? 'px-2 py-1 bg-emerald-400/60 rounded-3xl '
+              : 'hover:text-emerald-700'
+          } `}
         >
           FAQs
         </Link>
