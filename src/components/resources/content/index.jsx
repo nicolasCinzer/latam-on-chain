d647deeced180ec4c60b79dfdfca2feacf0adc01
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-18 18:32:08 -0300

    .

--- src/components/resources/content/index.jsx
+++ src/components/resources/content/index.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 import ScrollToTop from '../../utils/scrollToTop';
 
 /* Posts */
-import Post1 from './posts/que-es-blockchain';
+
 
 const Content = ({ params }) => {
   const { id } = params;
 
   return (
     <div className='py-8 pl-12 pr-16'>
-      {id === 'que-es-blockchain' ? <Post1 /> : <></>}
+      {id === 'que-es-blockchain' ? <div>blokchain</div> : <></>}
       {id === 'por-que-es-buena-idea-aprender-solidity-para-este-2023' ? (
         <div>solidity</div>
       ) : (
