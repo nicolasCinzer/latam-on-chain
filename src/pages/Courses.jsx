73e05fc94ee49a27ea070d0167dba6d4704a83e8
nicolasCinzer <nicolas.cinzer00@gmail.com>
2022-09-28 23:45:22 -0300

    fetchs success

--- src/pages/Courses.jsx
+++ src/pages/Courses.jsx
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import courses from '../services/coursesArray';
+import fetchCourses from '../services/fetchCourses';
 import Course from '../components/course';
 
 const Courses = () => {
+  const [courses, setCourses] = useState(null);
+
+  useEffect(() => {
+    fetchCourses().then((res) => {
+      console.log(res);
+      setCourses(res);
+    });
+  }, []);
+
   return (
     <div className='grid p-16 gap-y-8 xl:grid-cols-2 2xl:grid-cols-3 place-items-center'>
-      {courses.map(({ url, title, stack, description, language }) => (
-        <Course
-          key={title}
-          url={url}
-          title={title}
-          stack={stack}
-          description={description}
-          language={language}
-        />
-      ))}
+      {courses ? (
+        courses.map(({ url, title, stack, description, language }) => (
+          <Course
+            key={title}
+            url={url}
+            title={title}
+            stack={stack}
+            description={description}
+            language={language}
+          />
+        ))
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,11 +1,19 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Resource from '../components/resources/resource';
 import Topics from '../components/resources/topics';
-import resources from '../services/resourcesArray';
+import fetchResources from '../services/fetchResources';
 
 const Resources = () => {
   const [filter, setFilter] = useState('All');
+  const [resources, setResources] = useState(null);
+
+  useEffect(() => {
+    fetchResources().then((res) => {
+      console.log(res);
+      setResources(res);
+    });
+  }, []);
 
   return (
     <>
@@ -13,20 +21,24 @@ const Resources = () => {
         {filter !== 'All' ? `Posteos sobre ${filter}` : 'Ultimos Posteos'}
       </div>
       <div className='grid grid-cols-1 2xl:grid-cols-2 2xl:w-4/5 px-4'>
-        {resources.map(({ id, topic, icon, title, date, hosted }) =>
-          filter === 'All' || filter === topic ? (
-            <Resource
-              key={id}
-              id={id}
-              topic={topic}
-              icon={icon}
-              title={title}
-              date={date}
-              hosted={hosted}
-            />
-          ) : (
-            <></>
+        {resources ? (
+          resources.map(({ id, topic, icon, title, date, hosted }) =>
+            filter === 'All' || filter === topic ? (
+              <Resource
+                key={id}
+                id={id}
+                topic={topic}
+                icon={icon}
+                title={title}
+                date={date}
+                hosted={hosted}
+              />
+            ) : (
+              <></>
+            )
           )
+        ) : (
+          <></>
         )}
       </div>
       <Topics setFilter={setFilter} />
